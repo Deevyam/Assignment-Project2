afb825a2315981db6cff7c6fdb5abb095f2cda78
deevyam <101313416+Deevyam@users.noreply.github.com>
2024-11-04 13:06:46 +0530

    Update index.js
    Aggregate weather routes

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const weatherRoutes = require('./Routes/routes');
+
 
 dotenv.config();
 
 // MongoDB connection URI
 const MONGO_URI = "mongodb+srv://deevyamsoin:YNfBG7OPTil6gLAo@mernproject.g44gtwe.mongodb.net/?retryWrites=true&w=majority";
 
+// Define the server port
+const PORT = process.env.PORT || 5002;
+
 // Connect to MongoDB
 const connectDB = async () => {
-    mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-      console.log('Connected to MongoDB!');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+      mongoose
+      .connect(MONGO_URI)
+      .then(() => {
+        console.log('Connected to MongoDB!');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
 // Create Express app
 const app = express();
@@ -25,9 +30,12 @@ const app = express();
 // Connect to database
 connectDB();
 
+// Middleware
+app.use(express.json());
+
 // Define routes
 app.get('/', (req, res) => res.send('Weather App'));
+app.use('/api/weather', weatherRoutes);
 
 // Start the server
-const PORT = 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
